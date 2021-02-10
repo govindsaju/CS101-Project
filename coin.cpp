aa6dc881f54880735edb3c207e8418c7c2b08c3f
govindsaju <govindsaju2002@gmail.com>
2021-02-10 11:53:32 +0530

    "changed x coordinate range for coin spawning"

--- coin.cpp
+++ coin.cpp
@@ -39,7 +39,7 @@ void Coin::resetCoin() {
 void Coin::startx()
 {
   if (coin_level==0) coin_start_x=(PLAY_X_START+WINDOW_X)/2;          //for level 0, starting location is from a fixed point
-  else coin_start_x= PLAY_X_START+rand()%(WINDOW_X-PLAY_X_START);     //for level 1 and above, coin is launched from a random point
+  else coin_start_x= PLAY_X_START+100+rand()%(WINDOW_X-PLAY_X_START-100);     //for level 1 and above, coin is launched from a random point
 }
 //end of startx
 
